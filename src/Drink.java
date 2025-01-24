a1a4382eb309678d576629b90e9d44dc9f1875d2
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2025-01-24 10:59:40 +0300

    Create Drink.java

---
+++ src/Drink.java
@@ -0,0 +1,10 @@
+// Класс для представления напитка
+public class Drink {
+    String name; // Название напитка
+    int price;   // Цена напитка
+
+    public Drink(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
